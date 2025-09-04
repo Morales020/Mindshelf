3d4ac9b0474f5a2df86b3e6fb24d545555e9de6f
Tamem Abdrabou <tamemabdo21@gmail.com>
2025-09-04 12:51:40 +0300

    Added Event Dto's

---
+++ MindShelf_PL/MindShelf_BL/Dtos/Event/EventDetailsDto.cs
@@ -0,0 +1,24 @@
+﻿using MindShelf_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindShelf_BL.Dtos.Event
+{
+    internal class EventDetailsDto
+    {
+        public int EventId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartingDate { get; set; }
+        public DateTime EndingDate { get; set; }
+        public string Location { get; set; }
+        public bool IsOnline { get; set; }
+        public bool IsActive { get; set; }
+
+        // Include registrations if needed
+        //public List<EventRegistrationResponseDto> Registrations { get; set; }
+    }
+}
---
+++ MindShelf_PL/MindShelf_BL/Dtos/Event/EventResponseDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindShelf_BL.Dtos.Event
+{
+    internal class EventResponseDto
+    {
+        public int EventId { get; set; }
+        public string Title { get; set; }
+        public DateTime StartingDate { get; set; }
+        public DateTime EndingDate { get; set; }
+        public string Location { get; set; }
+        public bool IsOnline { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
