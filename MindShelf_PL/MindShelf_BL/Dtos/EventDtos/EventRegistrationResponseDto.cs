95259b783bb4fc95d7e9c1d93957703c7dd3a0df
Hossam Fathy <121311553+finitemist@users.noreply.github.com>
2025-09-04 14:58:58 +0300

    fix: Modifying EventRegistrationResponseDto

--- MindShelf_PL/MindShelf_BL/Dtos/EventDtos/EventRegistrationResponseDto.cs
+++ MindShelf_PL/MindShelf_BL/Dtos/EventDtos/EventRegistrationResponseDto.cs
@@ -4,6 +4,8 @@ public class EventRegistrationResponseDto
 {
     public int EventRegistrationId { get; set; }
     public DateTime RegistrationDate { get; set; }
+    public string UserId { get; set; }
+
     public string? Notes { get; set; }
     public string UserName { get; set; }
     public int EventId { get; set; }
