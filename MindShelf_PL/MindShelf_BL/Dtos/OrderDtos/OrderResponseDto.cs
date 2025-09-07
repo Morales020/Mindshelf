d46afff0e5a925868037dfef2c3afdf2619b0c1f
Hossam Fathy <121311553+finitemist@users.noreply.github.com>
2025-09-07 12:04:47 +0300

    fix: Mapping in the order services

--- MindShelf_PL/MindShelf_BL/Dtos/OrderDtos/OrderResponseDto.cs
+++ MindShelf_PL/MindShelf_BL/Dtos/OrderDtos/OrderResponseDto.cs
@@ -12,7 +12,7 @@ namespace MindShelf_BL.Dtos.OrderDtos
         public int Id { get; set; }
         public string UserId { get; set; }
         public string UserName { get; set; }
-        public string ShippingAddress { get; set; }
+        public string ShippingAddress { get; set; } = string.Empty;
         public DateTime OrderDate { get; set; } = DateTime.UtcNow;
         public OrderState OrderStatus { get; set; } // e.g., Pending, Shipped, Delivered, Cancelled
         public decimal Discount { get; set; }
